8a6c113a3620c42aa34e4c9df1a559c69ff790b6
thisisaaronland <straup@gmail.com>
2016-09-29 18:33:02 +0000

    start working on Transform for bild

--- image/bild.go
+++ image/bild.go
@@ -2,6 +2,7 @@ package image
 
 import (
 	"bytes"
+	"github.com/anthonynsimon/bild/transform"
 	iiifconfig "github.com/thisisaaronland/go-iiif/config"
 	iiifsource "github.com/thisisaaronland/go-iiif/source"
 	"image"
@@ -110,6 +111,38 @@ func (im *BILDImage) Dimensions() (Dimensions, error) {
 }
 
 func (im *BILDImage) Transform(t *Transformation) error {
+
+	if t.Region != "full" {
+
+		rgi, err := t.RegionInstructions(im)
+
+		if err != nil {
+			return err
+		}
+
+		box := image.Rect(rgi.X, rgi.Y, rgi.Width, rgi.Height)
+		crop := transform.Crop(im.image, box)
+
+		im.image = crop
+	}
+
+	if t.Size != "max" && t.Size != "full" {
+
+		si, err := t.SizeInstructions(im)
+
+		if err != nil {
+			return err
+		}
+
+		resized := transform.Resize(im.image, si.Width, si.Height, transform.Linear)
+		im.image = resized
+	}
+
+	_, err := t.RotationInstructions(im)
+
+	if err != nil {
+		return nil
+	}
 
 	return nil
 }
